d78e1cdbf0a1a2e00bb30f893bbea58781ab4003
Rasmus Leissner <rleissner@irhh.de>
2015-06-26 13:44:41 +0200

    fixed php empty query bug, added 404

--- index.php
+++ index.php
@@ -1,20 +1,29 @@
 <?php
 //$title = 'Index';
-$q = $_GET['q'];
+if(!empty($_GET['q'])){
+	$q = $_GET['q'];
+}
+else {
+	$q = 'Home';
+}
 $root = $_SERVER['DOCUMENT_ROOT'];
 include_once($root.'/dist/inc/head.php');
 include_once($root.'/dist/inc/nav.php');
 
-
-if (!empty($q)){
-	include_once($root.'/content/'.$q.'.html');
+if (!empty($q) && ($q != 'Home')) {
+	if (file_exists($root.'/content/'.$q.'.html')) {
+		include_once($root.'/content/'.$q.'.html');
+	}
+	else {
+		include_once($root.'/content/404.html');
+	}
 } 
 else {
 	include_once($root.'/content/index.html');
 }
 
 include_once($root.'/dist/inc/footer.php');
-include_once($root.'/dist/inc/script.php');
+include_once($root.'/dist/inc/scripts.php');
 ?>
 
 

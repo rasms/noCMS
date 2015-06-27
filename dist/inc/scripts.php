f665a69287fd37a9a91ec5d5b2a52940bb603d0a
Rasmus Leissner <rleissner@irhh.de>
2015-06-27 19:32:09 +0200

    switched for own jquery, changed index to home

--- dist/inc/scripts.php
+++ dist/inc/scripts.php
@@ -1,8 +1,8 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="https://cdn.irhh.de/jquery/jquery-1.11.3.min.js"></script>
     <script src="/dist/js/bootstrap.min.js"></script>
-   
+
   </body>
 </html>
--- index.php
+++ index.php
@@ -1,29 +1,26 @@
 <?php
-//$title = 'Index';
 if(!empty($_GET['q'])){
 	$q = $_GET['q'];
 }
 else {
-	$q = 'Home';
+	$q = 'home';
 }
 $root = $_SERVER['DOCUMENT_ROOT'];
 include_once($root.'/dist/inc/head.php');
 include_once($root.'/dist/inc/nav.php');
 
-if (!empty($q) && ($q != 'Home')) {
+if (!empty($q) && ($q != 'home')) {
 	if (file_exists($root.'/content/'.$q.'.html')) {
 		include_once($root.'/content/'.$q.'.html');
 	}
 	else {
 		include_once($root.'/content/404.html');
 	}
-} 
+}
 else {
-	include_once($root.'/content/index.html');
+	include_once($root.'/content/home.html');
 }
 
 include_once($root.'/dist/inc/footer.php');
 include_once($root.'/dist/inc/scripts.php');
 ?>
-
-

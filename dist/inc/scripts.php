a3ba6dc5c59f1f868dc29a2836205da01b408934
Rasmus Leissner <rleissner@irhh.de>
2015-06-28 17:44:49 +0200

    slimmed down content-logic, added better title-support, cleanup

--- dist/inc/scripts.php
+++ dist/inc/scripts.php
@@ -1,8 +1,4 @@
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://cdn.irhh.de/jquery/jquery-1.11.3.min.js"></script>
     <script src="/dist/js/bootstrap.min.js"></script>
-
   </body>
 </html>
--- index.php
+++ index.php
@@ -1,16 +1,13 @@
 <?php
+
 //get querys
+$f = '';
+$q = '';
 if(!empty($_GET['f'])){
 	$f = $_GET['f'];
 }
-else {
-	$f = '';
-}
 if(!empty($_GET['q'])){
 	$q = $_GET['q'];
-}
-else {
-	$q = 'home';
 }
 
 //include header and nav
@@ -19,28 +16,21 @@ include_once($root.'/dist/inc/head.php');
 include_once($root.'/dist/inc/nav.php');
 
 //include content
-if (!empty($f) && !empty($q)) {
-	if (file_exists($root.'/content/'.$f.'/'.$q.'.html')) {
-		include_once($root.'/content/'.$f.'/'.$q.'.html');
-	}
-	else {
-		include_once($root.'/content/404.html');
-	}
+if (!empty($f) && !empty($q) && (file_exists($root.'/content/'.$f.'/'.$q.'.html'))) {
+	include_once($root.'/content/'.$f.'/'.$q.'.html');
 }
-elseif (empty($f) && !empty($q) && ($q != 'home')) {
-	if (file_exists($root.'/content/'.$q.'.html')) {
-		include_once($root.'/content/'.$q.'.html');
-	}
-	else {
-		include_once($root.'/content/404.html');
-	}
+elseif (empty($f) && !empty($q) && (file_exists($root.'/content/'.$q.'.html'))) {
+	include_once($root.'/content/'.$q.'.html');
 }
-else {
+elseif (empty($f) && empty($q)) {
 	include_once($root.'/content/home.html');
 }
-
+else {
+	include_once($root.'/content/404.html');
+}
 
 //include footer and scripts
 include_once($root.'/dist/inc/footer.php');
 include_once($root.'/dist/inc/scripts.php');
+
 ?>
